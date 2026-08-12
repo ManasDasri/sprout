25ad17658f09de6dc6ca3cffbcfad825886766ab
Manas Ganesh Dasari <dasarimanas049@gmail.com>
2026-08-12 09:27:27 +0530

    Add: ability to read directories

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
 import "fmt"
+import "os" 
 
 func main() {
 	fmt.Println("sprout 🌱")
+	entries, err := os.ReadDir(".")
+	
+	if err != nil {
+		fmt.Println("Error",err)
+		return
+	}
+	
+	for _,entry := range entries{
+		fmt.Println(entry.Name())
+	}
 }
